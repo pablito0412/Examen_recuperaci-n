8bc1f7b7974bd196eb9b9ce098be557469a5e99f
pablito0412 <pablo200.a@gmail.com>
2024-02-23 12:17:55 -0500

    recuuperacion

---
+++ src/main/java/DUI/ExamenRecuperacion1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package DUI;
+
+/**
+ *
+ * @author pablo
+ */
+public class ExamenRecuperacion1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
